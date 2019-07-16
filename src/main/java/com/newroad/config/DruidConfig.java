5eb2ff788c1a0fce0e35f6fe011b1313ebd9d8ae
ohMiracle <zhengchangzccc@163.com>
2019-07-16 20:57:22 +0800

    2019/7/16

---
+++ src/main/java/com/newroad/config/DruidConfig.java
@@ -0,0 +1,37 @@
+package com.newroad.config;
+
+import com.alibaba.druid.filter.Filter;
+import com.alibaba.druid.filter.stat.StatFilter;
+import com.alibaba.druid.pool.DruidDataSource;
+import com.google.common.collect.Lists;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DruidConfig {
+    /*
+     * 将application.properties和类绑定
+     * 将bean方法的返回对象与外部的配置文件绑定
+     */
+    @ConfigurationProperties(prefix = "spring.druid")
+    @Bean(initMethod="init",destroyMethod = "close")
+    public DruidDataSource dataSource(){
+        DruidDataSource druidDataSource = new DruidDataSource();
+        //将慢日志的功能加入druid
+        druidDataSource.setProxyFilters(Lists.newArrayList(statFilter()));
+        return druidDataSource;
+    }
+
+    /**
+     * Druid自带的慢sqlfilter
+     * @return
+     */
+    @Bean
+    public Filter statFilter(){
+        StatFilter statFilter=new StatFilter();
+        statFilter.setSlowSqlMillis(5000);
+        statFilter.setMergeSql(true);
+        return statFilter;
+    }
+}
