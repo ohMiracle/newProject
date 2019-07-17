ed9339bf4ba9b0b4a1f2f59c567a19a4de8102c9
ohMiracle <zhengchangzccc@163.com>
2019-07-17 23:30:08 +0800

    添加了httpclient

--- src/test/java/com/newroad/HouseplatformApplicationTests.java
+++ src/test/java/com/newroad/HouseplatformApplicationTests.java
@@ -1,16 +1,27 @@
 package com.newroad;
 
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.util.EntityUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class HouseplatformApplicationTests {
-
+	@Autowired
+	private HttpClient httpClient;
 	@Test
 	public void contextLoads() {
 	}
-
+	@Test
+	public  void testHttpClient() throws ClientProtocolException,IOException{
+		System.out.println(EntityUtils.toString(httpClient.execute(new HttpGet("http://www.baidu.com")).getEntity()));
+	}
 }
